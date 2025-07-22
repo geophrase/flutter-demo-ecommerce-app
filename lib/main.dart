431eea5e8cf8556ab2b75b1b029cb998e4de9fd9
Iqbal Hussain <lincoln@live.in>
2025-07-22 21:52:22 +0530

    Homepage UI done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo_ecommerce_app/ui/pages/home/home_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -15,111 +17,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Demo Ecommerce Home'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 16),
-        Align(
-          alignment: Alignment.centerRight,
-          child: Padding(
-            padding: const EdgeInsets.only(right: 32),
-            child: OutlinedButton.icon(
-                onPressed: () => const {},
-                icon: const Icon(Icons.login),
-                label: const Text('Sign In'),
-                iconAlignment: IconAlignment.start,
-              ),
-          )),
-            SizedBox(height: 32,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              spacing: 16,
-              children: [
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      width: 150,
-                      height: 300,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/tshirt.png'),
-                          SizedBox(height: 8,),
-                          Text('T-Shirt', textAlign: TextAlign.center,),
-                          SizedBox(height: 8,),
-                          Text('₹190', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
-                          SizedBox(height: 8,),
-                          OutlinedButton.icon(
-                            onPressed: () => {},
-                            icon: const Icon(Icons.add_shopping_cart),
-                            label: const Text('Add to cart'),
-                            iconAlignment: IconAlignment.start,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      width: 150,
-                      height: 300,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/shoe.png'),
-                          SizedBox(height: 8,),
-                          Text('Shoe', textAlign: TextAlign.center,),
-                          SizedBox(height: 8,),
-                          Text('₹400', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
-                          SizedBox(height: 8,),
-                          OutlinedButton.icon(
-                            onPressed: () => {},
-                            icon: const Icon(Icons.add_shopping_cart),
-                            label: const Text('Add to cart'),
-                            iconAlignment: IconAlignment.start,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 32,),
-            FilledButton.icon(
-              onPressed: () => {},
-              icon: const Icon(Icons.add_shopping_cart),
-              label: const Text('Go to cart'),
-              iconAlignment: IconAlignment.start,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/ui/pages/home/home_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+final List<Map<String, dynamic>> items = [
+  {'name': 'T-Shirt', 'price': 190, 'image': 'tshirt'},
+  {'name': 'Shoe', 'price': 400, 'image': 'shoe'},
+];
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 16),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 32),
+                child: OutlinedButton.icon(
+                  onPressed: () => const {},
+                  icon: const Icon(Icons.login),
+                  label: const Text('Sign In'),
+                  iconAlignment: IconAlignment.start,
+                ),
+              ),
+            ),
+            SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 16,
+              children: [
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.center, // Center horizontally
+                    child: SizedBox(
+                      height: 300,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          final item = items[index];
+
+                          return Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SizedBox(
+                                width: 150,
+                                height: 300,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset('assets/${item['image']}.png'),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      item['name'],
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      "₹${item['price'].toString()}",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(height: 8),
+                                    OutlinedButton.icon(
+                                      onPressed: () => {},
+                                      icon: const Icon(Icons.add_shopping_cart),
+                                      label: const Text('Add to cart'),
+                                      iconAlignment: IconAlignment.start,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 32),
+            FilledButton.icon(
+              onPressed: () => {},
+              icon: const Icon(Icons.shopping_cart),
+              label: const Text('Go to cart'),
+              iconAlignment: IconAlignment.start,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
